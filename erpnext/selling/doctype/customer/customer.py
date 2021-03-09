f6af711d866996c3e30fe1af113e97d44b8b0b40
Caleb Kandoro <kandoroc@hotmail.com>
2021-03-09 16:26:19 +0200

    credit referral message improvements

--- erpnext/selling/doctype/customer/customer.py
+++ erpnext/selling/doctype/customer/customer.py
@@ -344,8 +344,9 @@ def check_credit_limit(customer, company, ignore_outstanding_sales_order=False, 
 		# If not authorized person raise exception
 		credit_controller = frappe.db.get_value('Accounts Settings', None, 'credit_controller')
 		if not credit_controller or credit_controller not in frappe.get_roles():
-			throw(_("Please contact to the user who have Sales Master Manager {0} role")
-				.format(" / " + credit_controller if credit_controller else ""))
+			throw("The order has been credit referred")
+   			# throw(_("Please contact to the user who have Sales Master Manager {0} role")
+			# 	.format(" / " + credit_controller if credit_controller else ""))
 
 def get_customer_outstanding(customer, company, ignore_outstanding_sales_order=False, cost_center=None):
 	# Outstanding based on GL Entries
