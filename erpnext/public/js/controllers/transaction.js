b4af0f06c59b2282e5cbde55cae2e12fd301df75
Caleb Kandoro <kandoroc@hotmail.com>
2023-04-07 10:15:16 +0200

    defaulting quantities to zero

--- erpnext/public/js/controllers/transaction.js
+++ erpnext/public/js/controllers/transaction.js
@@ -520,15 +520,12 @@ erpnext.TransactionController = erpnext.taxes_and_totals.extend({
 								},
 								() => me.conversion_factor(doc, cdt, cdn, true),
 								() => me.remove_pricing_rule(item),
-								/** Goprime 2021
-								 *  Removed default to zero to ensure quantity field in item filter works on quotations
-								 */
-								// () => {
-								// 	if(cdt != "Quotation Item") { return }
-								// 	var d = locals[cdt][cdn]
-								// 	d.qty = 0
-								// 	me.frm.refresh_field('items')
-								// }
+								() => {
+									if(cdt != "Quotation Item") { return }
+									var d = locals[cdt][cdn]
+									d.qty = 0
+									me.frm.refresh_field('items')
+								}
 							]);
 						}
 					}
